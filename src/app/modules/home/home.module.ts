5620217fa4264bef8194ca99a15582b321ea0f76
Hugo REY <hrey222@gmail.com>
2024-01-19 10:07:07 +0100

    feat: home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { LogoIconComponent } from './logo-icon/logo-icon.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, LogoIconComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
