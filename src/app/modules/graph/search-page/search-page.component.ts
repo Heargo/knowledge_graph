3f8d485132914c1ea4fa34779074883cd2ba3f74
Simon LEONARD <git-1001af4@sinux.sh>
2024-01-19 09:24:20 +0100

    feat(search): add options of ingredients (WIP)

--- src/app/modules/graph/search-page/search-page.component.ts
+++ src/app/modules/graph/search-page/search-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { GraphService } from '../services/graph.service';
+import { Ingredients } from '../models/graph.models';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  styleUrls: ['./search-page.component.scss'],
 })
 export class SearchPageComponent {
+  protected ingredients: string[] = [];
+  constructor(private readonly graphService: GraphService) {
+    this.ingredients = graphService.getAllIngredients();
+  }
 
+  //WIP
+  getByIngredients(ingredients: string[]) {
+    ingredients.forEach(ingredient => {
+      this.graphService.getCocktailByIngredient(ingredient);
+    });
+  }
 }
